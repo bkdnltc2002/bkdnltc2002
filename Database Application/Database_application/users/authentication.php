614cc9b488760b4d23faec07b2d404290656aa25
Thanh Nguyen <95325717+bkdnltc2002@users.noreply.github.com>
2022-09-13 20:15:43 +0700

    Delete Database Application directory

--- Database Application/Database_application/users/authentication.php
+++
@@ -1,6 +0,0 @@
-<?php
-  if($_SESSION["user"]["type"] != $pageType) {
-    header("Location: ../login.php");
-    exit();
-  }
-?>
--- Database Application/Database_application/users/customer/customer_auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-  session_start();
-  if($_SESSION["user"]["type"] != "customer") {
-    header("Location: ../login.php");
-    exit();
-  }
-
-  
-?>
